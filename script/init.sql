e49a57a7628829fa871b7e57ee5501348463df4a
ramdos0207 <dev@ramdos.net>
2023-12-11 18:10:48 +0900

    add possibility table

--- script/init.sql
+++ script/init.sql
@@ -6,11 +6,16 @@ CREATE TABLE IF NOT EXISTS `task` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` text NOT NULL,
   `description` text NOT NULL,
-  `possibility` int(11) NOT NULL,
+  `possibility_id` int(11) NOT NULL,
   `difficulty` int(11) NOT NULL,
   `created_at` date NOT NULL,
   `updated_at` date NOT NULL,
   `due_date` date NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-INSERT INTO `task` (`user`,`id`,`title`,`description`,`possibility`,`difficulty`,`created_at`,`updated_at`,`due_date`) VALUES ('ramdos',1,'電磁気学の課題','第二回の講義までにやる',2,3,'2023-12-01','2023-12-05','2023-12-10');
+CREATE TABLE IF NOT EXISTS `possibility` (
+  `possibility_id` int(11) NOT NULL AUTO_INCREMENT,
+  `possibility` string NOT NULL,
+  PRIMARY KEY (`possibility_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `task` (`user`,`id`,`title`,`description`,`possibility_id`,`difficulty`,`created_at`,`updated_at`,`due_date`) VALUES ('ramdos',1,'電磁気学の課題','第二回の講義までにやる',2,3,'2023-12-01','2023-12-05','2023-12-10');
