0f0fcf8e047115a2d3ca782dc2f94fd3cefec618
Pina641 <kakinotane2048@gmail.com>
2023-12-13 19:02:35 +0900

    POST /task を追加

--- src/task.go
+++ src/task.go
@@ -29,3 +29,39 @@ func getTaskHandler(c echo.Context) error {
 	}
 	return c.JSON(http.StatusOK, res)
 }
+
+func postTaskHandler(c echo.Context) error {
+	var payload AuthHeader
+	(&echo.DefaultBinder{}).BindHeaders(c, &payload)
+	userId := payload.UserId
+
+	var newTask TaskWithoutId
+	if err := c.Bind(&newTask); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request payload"})
+	}
+
+	res, err := db.Exec("INSERT INTO task (title, description, condition_id, difficulty, due_date, user) VALUES (?,?,?,?,?,?)", newTask.Title, newTask.Description, newTask.ConditionId, newTask.Difficulty, newTask.DueDate, userId)
+
+	if err != nil {
+		fmt.Println(err)
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to add task"})
+	}
+
+	taskId, err := res.LastInsertId()
+	if err != nil {
+		fmt.Println(err)
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to get task ID"})
+	}
+
+	addedTask := TaskRes{
+		Id:          int(taskId),
+		Title:       newTask.Title,
+		Description: newTask.Description,
+		ConditionId: newTask.ConditionId,
+		Difficulty:  newTask.Difficulty,
+		DueDate:     newTask.DueDate,
+		User:        userId,
+	}
+
+	return c.JSON(http.StatusCreated, addedTask)
+}
