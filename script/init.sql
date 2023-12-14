bd26470717a78da6d3ca05742f28cabfaf9dad9f
ramdos0207 <dev@ramdos.net>
2023-12-14 16:50:49 +0900

    fix init.sql

--- script/init.sql
+++ script/init.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE IF NOT EXISTS `yaranai`;
 USE `yaranai`;
 CREATE USER yaranai IDENTIFIED BY 'password';
+grant all on *.* to yaranai;
 CREATE TABLE `task` (
   `user` text NOT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -18,5 +19,5 @@ CREATE TABLE `condition` (
   `condition` text NOT NULL,
   PRIMARY KEY (`condition_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-ALTER TABLE `task` ADD FOREIGN KEY (`condition_id`) REFERENCES `condition`(`condition_id`);
+-- ALTER TABLE `task` ADD FOREIGN KEY (`condition_id`) REFERENCES `condition`(`condition_id`);
 INSERT INTO `task` (`user`,`id`,`title`,`description`,`condition_id`,`difficulty`,`created_at`,`updated_at`,`dueDate`) VALUES ('ramdos',1,'電磁気学の課題','第二回の講義までにやる',2,3,'2023-12-01','2023-12-05','2023-12-10');
