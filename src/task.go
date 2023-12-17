53a806e1be6d1adbd6c89039e6232c7a2b6fecb0
comavius <comavius@proton.me>
2023-12-17 14:41:24 +0900

    fixed typo

--- src/task.go
+++ src/task.go
@@ -146,7 +146,7 @@ func deleteTaskHandler(c echo.Context) error {
 		DueDate:       task.DueDate,
 		DeletedAtUnix: dateOfNow.Unix(),
 	}
-	_, err = db.Exec("INSERT INTO deletd_task (user, id, condition_id, created_at, due_date, deleted_at_unix) VALUES (?,?,?,?,?,?)", deleting_task.User, deleting_task.Id, deleting_task.ConditionId, deleting_task.CreatedAt, deleting_task.DueDate, deleting_task.DeletedAtUnix)
+	_, err = db.Exec("INSERT INTO deleted_task (user, id, condition_id, created_at, due_date, deleted_at_unix) VALUES (?,?,?,?,?,?)", deleting_task.User, deleting_task.Id, deleting_task.ConditionId, deleting_task.CreatedAt, deleting_task.DueDate, deleting_task.DeletedAtUnix)
 	if err != nil {
 		fmt.Println(err)
 		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to execute database query"})
